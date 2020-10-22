517a1ca602ec091bf0b23a122324aca0ebe702d6
swearwalker <swear.walker@protonmail.com>
2020-10-22 23:28:48 +0300

    Refactored

---
+++ src/components/sw-button/sw-button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>{{ title }}</button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { SwButtonDto } from "@/types";
+
+@Component
+export default class SwButton extends Vue implements SwButtonDto {
+  @Prop({ default: "Title button" })
+  readonly title?: string;
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/types/components/sw-button.dto.ts
@@ -0,0 +1,3 @@
+export interface SwButtonDto {
+    title?: string;
+}
